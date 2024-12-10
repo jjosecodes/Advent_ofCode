c5c8a4830bbe851e9334d24afc74d74ee9ccacee
jjosecodes <josejob123121@gmail.com>
2024-12-09 23:30:25 -0600

    forgot i was supposed to use python not c++

---
+++ day1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+using namespace std;
+const int N = 1000;
+
+int main (){
+    vector <int> a,b;
+
+    for ( int i = 0; i <N; i++){
+        int x,y;
+        a.push_back(x);
+        b.push_back(y);
+
+    }
+
+    sort(a.begin(),a.end());
+    sort(b.begin(),b.end());
+
+ 
+
+    return 0;
+}
